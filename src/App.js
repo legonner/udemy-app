8b594c70ca7224c8b13a01eca3f5bd45de10b0f4
Max <legonner@gmail.com>
2024-02-06 16:19:31 +0200

    create react app

---
+++ src/App.js
@@ -0,0 +1,28 @@
+import logo from "./logo.svg";
+import "./App.css";
+
+const Header = () => {
+  return <h2>Hello World!</h2>
+}
+
+const Field = () => {
+  return <input placeholder="Type here" type="text"/>
+}
+
+function Btn() {
+  const text = 'Log in';
+  const logged = true;
+  return <button>{logged ? 'Enter' : text}</button>
+}
+
+function App() {
+  return (
+    <div className="App">
+      <Header/>
+      <Field/>
+      <Btn/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+
+// const elem = <h2>Hello world!</h2>;
+const elem = React.createElement('h2', null, 'Hello world!!!');
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <App/>
+);
