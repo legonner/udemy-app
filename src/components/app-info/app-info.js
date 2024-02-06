5b34159176628cc56a270c636e4ba3edb0b3d029
Max <legonner@gmail.com>
2024-02-06 21:26:06 +0200

    added component content

---
+++ src/components/app-info/app-info.js
@@ -0,0 +1,13 @@
+import "./app-info.css";
+
+const AppInfo = () => {
+  return (
+    <div className="app-info">
+      <h1>Employee accounting at <b>Enigma</b></h1>
+      <h2>Total number of employees: </h2>
+      <h2>The award goes to:</h2>
+    </div>
+  );
+};
+
+export default AppInfo;
--- src/components/app/app.js
+++ src/components/app/app.js
@@ -1,11 +1,24 @@
-import './app.css';
+import AppInfo from "../app-info/app-info";
+import SearchPanel from "../search-panel/search-panel";
+import AppFilter from "../app-filter/app-filter";
+import EmployeesList from "../employees-list/employees-list";
+import EmployeesAddForm from "../employees-add-form/employees-add-form";
+
+import "./app.css";
 
 function App() {
-    return (
-        <div className="app">
+  return (
+    <div className="app">
+      <AppInfo />
 
-        </div>
-    );
+      <div className="search-panel">
+        <SearchPanel />
+        <AppFilter />
+      </div>
+      <EmployeesList />
+      <EmployeesAddForm />
+    </div>
+  );
 }
 
 export default App;
